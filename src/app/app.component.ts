2ec3cbcef911fc755736c789196664da9cbb1abe
Zayan27 <mzayankhan27@gmail.com>
2025-07-22 23:04:44 +0500

    structure commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeCompComponent } from './components/home-comp/home-comp.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeCompComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'DwebSolutionsPortfolio';
+  
 }
---
+++ src/app/components/home-comp/home-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-comp.component.html',
+  styleUrl: './home-comp.component.scss'
+})
+export class HomeCompComponent {
+
+}
